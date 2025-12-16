84867ffa4764113474ca358c5316b8b8c19771a7
srinivas7877 <srinivastalari787@gmail.com>
2025-12-16 23:18:08 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 (async () => {
   await registerRoutes(httpServer, app);
 
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  app.use((err: Error & { status?: number; statusCode?: number }, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
